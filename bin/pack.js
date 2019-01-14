4cd4d6b3425e5f25d2fb54d7ce6838b5179453b7
Kevin Isom <kevin.isom@gmail.com>
2019-01-14 14:55:07 +1300

    update it for me
    No freaking typescript

--- bin/pack.js
+++ bin/pack.js
@@ -7,23 +7,25 @@ const resourcesPath = path.join(__dirname, '..', 'resources')
 
 packager({
   dir: path.join(__dirname, '..'),
-  appCopyright: '© 2019, Zihua Li',
+  appCopyright: '© 2019, Kevin Isom',
   asar: true,
   overwrite: true,
   electronVersion: pkg.electronVersion,
   icon: path.join(resourcesPath, 'icns', 'MyIcon'),
   out: path.join(__dirname, '..', 'dist', 'out'),
   platform: 'mas',
-  appBundleId: `li.zihua.${pkg.name}`,
+  appBundleId: `isom.kevin.${pkg.name}`,
   appCategoryType: 'public.app-category.developer-tools',
   osxSign: {
     type: process.env.NODE_ENV === 'production' ? 'distribution' : 'development',
     entitlements: path.join(resourcesPath, 'parent.plist'),
     'entitlements-inherit': path.join(resourcesPath, 'child.plist')
   }
+
 }).then((res) => {
   const app = path.join(res[0], `${pkg.productName}.app`)
   console.log('flating...', app)
+  process.exit(0);
   flat({ app }, function done (err) {
     if (err) {
       throw err
